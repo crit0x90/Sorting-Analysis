7bcc9d1e2cb38db1af71b4241208d5fd78cbb279
scottmccoy13 <scottmccoy13@gmail.com>
2018-01-27 15:20:38 -0800

    Added quick sort, modified the to do list

--- driver.cpp
+++ driver.cpp
@@ -14,8 +14,10 @@ int main()
 	printVector(v);
 	cout << endl;
 
+	vectorquicksort(v, 0, v.size()-1);
+
 	printVector(v);
-	cout << endl << verifySorted(v) << endl; //will print one if the vector is sorted
+	cout << endl << "Is sorted: " << verifySorted(v) << endl; //will print one if the vector is sorted
 
 	return 0;
 }
